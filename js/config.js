d066d218e4140f9231aa7a9c3f830d7105c82b90
Utkarsh Srivastava <>
2021-01-19 22:16:25 -0500

    Changed the user pool id

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
   cognito: {
     userPoolId: "us-east-1_Ksu3r8cHj", // e.g. us-east-2_uXboG5pAb
-    userPoolClientId: "3mo8bj1ulc54g31jsd5l44tik3", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    userPoolClientId: "23tiod3u4etfc60tlj42v44ne5", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
     region: "us-east-1", // e.g. us-east-2
   },
   api: {
